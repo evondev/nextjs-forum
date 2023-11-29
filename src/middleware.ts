5fb77bc8401ca84de5c467ec3582758f623acce7
evondev <trananhtuan400@gmail.com>
2023-11-29 21:07:34 +0700

    Add logging for request object in webhook route

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,12 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/", "/tags", "/post/(.*)", "/api/webhooks(.*)"],
-  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+.[w]+$)", "/api/uploadthing"],
+  publicRoutes: ["/", "/tags", "/post/(.*)", "/api/webhook"],
+  ignoredRoutes: [
+    "/((?!api|trpc))(_next.*|.+.[w]+$)",
+    "/api/uploadthing",
+    "/api/webhook",
+  ],
 });
 
 export const config = {
