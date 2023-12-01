a1541cd86fc779071a9849fbbad0b095bae47c85
evondev <trananhtuan400@gmail.com>
2023-12-02 01:06:01 +0700

    Update layout padding

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -7,7 +7,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <NavBar />
       <div className="max-w-[1440px] mx-auto grid lg:grid-cols-[260px_minmax(0,1fr)] gap-5 items-start">
         <LeftSidebar></LeftSidebar>
-        <div className="py-10 pr-5">{children}</div>
+        <div className="py-5 pr-5">{children}</div>
       </div>
     </>
   );
